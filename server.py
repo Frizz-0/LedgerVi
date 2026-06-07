ad772946e2d9edd727ef42ecbad0644961c1a8af
@FriZz_0 <bharathfrizz@gmail.com>
2026-06-07 12:47:33 +0100

    changed the port and host url

--- server.py
+++ server.py
@@ -307,4 +307,5 @@ async def purge_ledger_record(thread_id: str, admin_signature: str):
     except Exception as e: raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    # uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
